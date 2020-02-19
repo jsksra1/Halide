94e9ee049074554131f04b9249eef53561cdba78
Alex Reinking <alex_reinking@berkeley.edu>
2020-02-19 01:59:33 -0800

    Adding HalideConfig.cmake. Setting version to 1.0.0!

---
+++ packaging/HalideConfig.cmake
@@ -0,0 +1,30 @@
+set(known_components PNG JPEG)
+
+if (${CMAKE_FIND_PACKAGE_NAME}_FIND_COMPONENTS)
+    set(comps ${${CMAKE_FIND_PACKAGE_NAME}_FIND_COMPONENTS})
+else ()
+    set(comps ${known_components})
+endif ()
+
+include("${CMAKE_CURRENT_LIST_DIR}/Halide-Targets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/HalideGeneratorHelpers.cmake")
+
+foreach (comp IN LISTS ${CMAKE_FIND_PACKAGE_NAME}_comps)
+    if (NOT ${comp} IN_LIST known_components)
+        set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE
+            "Halide does not recognize requested component: ${comp}")
+        set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
+        return()
+    endif ()
+
+    if (NOT TARGET ${comp}::${comp})
+        unset(extraArgs)
+        if (${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
+            list(APPEND extraArgs QUIET)
+        endif ()
+        if (${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED_${comp})
+            list(APPEND extraArgs REQUIRED)
+        endif ()
+        find_package(${comp} ${extraArgs})
+    endif ()
+endforeach ()
